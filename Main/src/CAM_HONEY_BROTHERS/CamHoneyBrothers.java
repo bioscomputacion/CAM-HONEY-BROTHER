0a6c96ba78d89e556538834c9d380ceb4f69e187
Caco <Caco@ADMINISTRACION-CACO>
2021-09-14 20:37:57 -0300

    first commit

--- Main/src/CAM_HONEY_BROTHERS/CamHoneyBrothers.java
+++ Main/src/CAM_HONEY_BROTHERS/CamHoneyBrothers.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+package CAM_HONEY_BROTHERS;
 
 /**
  *
  * @author Caco
  */
-public class Main {
+public class CamHoneyBrothers {
 
     /**
      * @param args the command line arguments
